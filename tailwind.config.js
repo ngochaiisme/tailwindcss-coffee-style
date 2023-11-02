379ddc419aa2a6a3da214b5a1c18d98ddde699d3
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-02 11:11:19 +0700

    Add slider

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,29 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: ['./public/**/*.{html,js}'],
-    theme: {
-        extend: {
-            colors: {
-                'light-coffee': '#C89F94',
-            },
-            fontFamily: {
-                Karla: ['Karla', 'sans-serif'],
-            },
-            keyframes: {
-                wiggle: {
-                    '0%, 100%': { transform: 'rotate(-3deg)' },
-                    '50%': { transform: 'rotate(3deg)' },
-                },
-                slideDown: {
-                    '0%': { transform: 'translateY(-100%)' },
-                    '100%': { transform: 'translateY(0)' },
-                },
-            },
-            animation: {
-                wiggle: 'wiggle 1s ease-in-out infinite',
-                slideDown: 'slideDown .4s ease-in-out',
-            },
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "light-coffee": "#C89F94"
+      },
+      fontFamily: {
+        Karla: ["Karla", "sans-serif"]
+      },
+      keyframes: {
+        wiggle: {
+          "0%, 100%": { transform: "rotate(-3deg)" },
+          "50%": { transform: "rotate(3deg)" }
         },
-    },
-    plugins: [],
-}
+        slideDown: {
+          "0%": { transform: "translateY(-100%)" },
+          "100%": { transform: "translateY(0)" }
+        }
+      },
+      animation: {
+        wiggle: "wiggle 1s ease-in-out infinite",
+        slideDown: "slideDown .4s ease-in-out"
+      },
+      backgroundImage: {
+        "slider-bg": 'url("./img/slider_background.jpg")'
+      }
+    }
+  },
+  plugins: []
+};
