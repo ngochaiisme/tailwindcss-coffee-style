e21b3d9111b8aeac334b485ee5e569058a267aac
Dương Ngọc Hải <dngochai0511@gmail.com>
2023-11-03 08:00:27 +0700

    Add Story & SubHeadline

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        "light-coffee": "#C89F94"
+        coffee: {
+          50: "#E8D6D0",
+          200: "#C89F94",
+          400: "#A25F4B",
+          600: "#744838"
+        }
       },
       fontFamily: {
         Karla: ["Karla", "sans-serif"]
